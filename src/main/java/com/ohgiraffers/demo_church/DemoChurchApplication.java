8ccee919d028a17c02ba4ff2700186e8f24ef390
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-14 16:47:26 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/demo_church/DemoChurchApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.demo_church;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoChurchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoChurchApplication.class, args);
+    }
+
+}
