08e88ee76b57d2d1d550d2904ca19dac573d828e
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-15 20:06:08 +0900

    feat: MainRepositoryTest

---
+++ src/test/java/com/ohgiraffers/demo_church/repository/MainRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.demo_church.repository;
+
+import com.ohgiraffers.demo_church.config.GoogleSheetConfig;
+import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+
+@SpringBootTest
+@Slf4j
+class MainRepositoryTest {
+
+    private final MainRepository mainRepository = new MainRepository(new GoogleSheetConfig());
+    @Value("${google.spreadsheet.id}")
+    String SPREAD_SHEET_ID;
+
+    @Value("${google.spreadsheet.main}!${google.spreadsheet.main.range}")
+    private String MAIN_SHEET_RANGE;
+
+    @Test
+    void findAll_실행시간_측정() {
+        long start = System.nanoTime();
+
+        List<Map<String, String>> result = mainRepository.findAll(SPREAD_SHEET_ID, MAIN_SHEET_RANGE);
+
+        long end = System.nanoTime();
+        double elapsedMillis = (end - start) / 1_000_000.0;
+
+        log.info("findAll() 실행 시간: {} ms", elapsedMillis);
+        log.info("가져온 행 수: {}", result.size());
+        log.info("MAIN_SHEET_RANGE: {} ", MAIN_SHEET_RANGE);
+    }
+}
