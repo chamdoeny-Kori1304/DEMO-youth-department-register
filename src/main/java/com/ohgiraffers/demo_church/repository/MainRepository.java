a25e0d847abb4ea5c87d55f2eda65f0d8f4c59fc
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-17 15:11:37 +0900

    feat: getTeamToMembersMap()
    팀으로 인원을 나눈 결과를 리턴

--- src/main/java/com/ohgiraffers/demo_church/repository/MainRepository.java
+++ src/main/java/com/ohgiraffers/demo_church/repository/MainRepository.java
@@ -63,7 +63,7 @@ public class MainRepository {
                 boolean isAdd = this.shouldAddRowToResponse(rowMap, defaultColumnName);
                 if (isAdd) {
                     findResponse.add(rowMap);
-                }else emptyRowCount++;
+                } else emptyRowCount++;
             }
 
             return findResponse;
@@ -71,6 +71,44 @@ public class MainRepository {
             log.error("Failed to read data from the spreadsheet", e);
             throw new RuntimeException("Failed to read data: " + e.getMessage(), e);
         }
+    }
+
+
+    public Map<String, List<String>> getTeamToMembersMap(String range, String defaultColumnName, String yearQuarter) {
+        Map<String, List<String>> teamToMembersMap = new HashMap<>();
+        int emptyRowCount = 0;
+
+        List<List<Object>> values = this.filterData(range);
+        if (values == null || values.isEmpty()) return Collections.emptyMap();
+
+        List<String> columnNames = getColumNames(values.get(0));
+
+        try {
+            for (int i = 1; i < values.size(); i++) {
+                List<Object> row = values.get(i);
+
+                if (emptyRowCount >= 2) break;
+
+                final Map<String, String> rowMap = this.createRowMapFromSheetData(columnNames, row);
+                boolean isAdd = this.shouldAddRowToResponse(rowMap, defaultColumnName);
+
+                if (isAdd) {
+                    String key = rowMap.get(yearQuarter);
+                    if (!teamToMembersMap.containsKey(key))
+                        teamToMembersMap.put(key, new ArrayList<>());
+
+                    List<String> list=teamToMembersMap.get(key);
+                    list.add(rowMap.get("이름"));
+                } else emptyRowCount++;
+            }
+
+            return teamToMembersMap;
+        } catch (Exception e) {
+            log.error("Failed to read data from the spreadsheet", e);
+            throw new RuntimeException("Failed to read data: " + e.getMessage(), e);
+        }
+
+
     }
 
     private List<List<Object>> filterData(String range) {
