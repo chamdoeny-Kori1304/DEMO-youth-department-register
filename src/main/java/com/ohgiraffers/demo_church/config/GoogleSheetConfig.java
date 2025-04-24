3d86072ee3b19c0524953463199eff43b093b45f
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-24 17:15:05 +0900

    feat:Add  GoogleSheetsClientProvider
    Moved Sheets.Builder logic out of GoogleSheetConfig to support better unit testing and maintainability.

--- src/main/java/com/ohgiraffers/demo_church/config/GoogleSheetConfig.java
+++ src/main/java/com/ohgiraffers/demo_church/config/GoogleSheetConfig.java
@@ -1,34 +1,21 @@
 package com.ohgiraffers.demo_church.config;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
+
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import com.ohgiraffers.demo_church.common.service.GoogleSheetsClientProvider;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+
+@Configuration
 public class GoogleSheetConfig {
-    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String CREDENTIALS_FILE_PATH = "/google_api_key.json";
-    private Sheets sheetsService;
+    private final GoogleSheetsClientProvider googleSheetsClientProvider;
 
-    public Sheets provideSheetsClient() throws IOException, GeneralSecurityException {
-        if (sheetsService == null) {
-            GoogleCredentials credential = GoogleCredentials.fromStream(
-                            new ClassPathResource(CREDENTIALS_FILE_PATH).getInputStream())
-                    .createScoped("https://www.googleapis.com/auth/spreadsheets");
+    public GoogleSheetConfig(GoogleSheetsClientProvider googleSheetsClientProvider) {
+        this.googleSheetsClientProvider = googleSheetsClientProvider;
+    }
 
-            sheetsService = new Sheets.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(),
-                    JSON_FACTORY,
-                    new HttpCredentialsAdapter(credential)
-            ).setApplicationName("uniflee").build();
-        }
-        return sheetsService;
+    public Sheets provideSheetsClient() {
+        return googleSheetsClientProvider.getClient();
     }
 }
