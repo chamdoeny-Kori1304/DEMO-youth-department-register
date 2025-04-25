54afa1e89e79fc9ef5b64d304aed56ba7a716cd6
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-25 22:37:08 +0900

    Test: Add Member data

--- src/test/java/com/ohgiraffers/demo_church/repository/MainRepositoryTest.java
+++ src/test/java/com/ohgiraffers/demo_church/repository/MainRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.ohgiraffers.demo_church.repository;
 
-import com.ohgiraffers.demo_church.config.GoogleSheetConfig;
+import com.ohgiraffers.demo_church.domain.Member;
+import com.ohgiraffers.demo_church.domain.enums.Gender;
 import org.junit.jupiter.api.Test;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,11 @@ class MainRepositoryTest {
     private int MEMBER_COUNT;
 
     private String MAIN_DEFAULT_COLUMN_NAME = "이름";
+
+    static Member member1 = new Member("user01", "김철수", "010-1234-5678", "서울시 강남구", Gender.MALE, LocalDate.of(1990, 5, 15), "친구", "조효린");
+    static Member member2 = new Member("user02", "이영희", "010-9876-5432", "경기도 성남시", Gender.FEMALE, LocalDate.of(1995, 10, 20), "동료", "성종민");
+    static Member member3 = new Member("user03", "박지성", "010-1111-2222", "인천광역시", Gender.MALE, LocalDate.of(1988, 7, 1), "가족", "박용진");
+    static Member member4 = new Member("user04", "최유나", "010-3333-4444", "대전광역시", Gender.FEMALE, LocalDate.of(2000, 3, 10), "선배", "정현빈");
 
     @Test
     void 모든_멤버_찾는데_걸리는_시간() {
@@ -53,17 +60,17 @@ class MainRepositoryTest {
 
     @Test
     void whenValidRange_thenReturnsMappedList() {
-        String range = "Sheet1!A1:C3"; // 실제 데이터가 있는 곳
+        String range = "Sheet1!A1:J3"; // 실제 데이터가 있는 곳
         List<Map<String, String>> result = mainRepository.findData(range, "");
 
         assertEquals(2, result.size()); // 헤더 제외한 row 2개
-        assertTrue(result.get(0).containsKey("1111")); // 헤더 예시
+        assertTrue(result.get(0).containsKey("현재 셀")); // 헤더 예시
     }
 
     @Test
     void whenEmptyDataRange_thenReturnsEmptyList() {
         String range = "Sheet1!Z1:Z3"; // 비어 있는 영역
-        List<Map<String, String>> result = mainRepository.findData(range,"");
+        List<Map<String, String>> result = mainRepository.findData(range, "");
 
         assertTrue(result.isEmpty());
     }
